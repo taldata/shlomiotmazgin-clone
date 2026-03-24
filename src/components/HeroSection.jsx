5ae3fe7ea9f939f7d16393d27e4c45b269baa4a7
taldata <sabag.tal@gmail.com>
2026-03-24 19:01:51 +0200

    refactor: Remove clipPath wrapper from HeroSection component and update hero image.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 export default function HeroSection() {
   return (
-    <section className="mt-[5.5rem] relative w-full overflow-hidden">
-      <div style={{ clipPath: 'inset(0 3% 10% 0)' }}>
-        <img
-          src="/hero-image.png"
-          alt="Transform your R&D 'Black Box' into a strategic ROI driver"
-          className="w-full h-auto block"
-        />
-      </div>
+    <section className="mt-[5.5rem] relative w-full">
+      <img
+        src="/hero-image.png"
+        alt="Transform your R&D 'Black Box' into a strategic ROI driver"
+        className="w-full h-auto block"
+      />
     </section>
   );
 }
